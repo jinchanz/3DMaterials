52cabcb92fc57373696d77e27edf1440d4c2808e
jinchanz <muscle.mark.cheng@gmail.com>
2023-04-18 19:25:44 +0800

    chore: custom plugins

--- build.lowcode.js
+++ build.lowcode.js
@@ -9,7 +9,25 @@ module.exports = {
       '@alifd/build-plugin-lowcode',
       {
         library,
-        engineScope: "@alilc"
+        engineScope: "@alilc",
+        staticResources: {
+          enginePresetCssUrl: 'https://alifd.alicdn.com/npm/@alifd/lowcode-preset-plugin@1.1.9-beta.0/dist/editor-preset-plugin.css',
+          enginePresetJsUrl: 'https://alifd.alicdn.com/npm/@alifd/lowcode-preset-plugin@1.1.9-beta.0/dist/editor-preset-plugin.js',
+        },
+        presetConfig: {
+          pluginConfig: {
+            logo: {
+              logo: 'https://mdn.alipayobjects.com/huamei_0prmtq/afts/img/A*pDJSTbz7wgAAAAAAAAAAAAAADvuFAQ/original'
+            }
+          },
+          simulatorUrl: [
+            'https://alifd.alicdn.com/npm/@alilc/lowcode-react-simulator-renderer@1.0.18/dist/js/react-simulator-renderer.js',
+            'https://alifd.alicdn.com/npm/@alilc/lowcode-react-simulator-renderer@1.0.18/dist/css/react-simulator-renderer.css',
+          ]
+        },
+        customPlugins: [
+          
+        ]
       },
     ],
   ],
